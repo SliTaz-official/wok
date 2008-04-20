63d48b7ec754c2744ee8c3b5b0fe867f009f35e8
Christophe Lincoln <pankso@slitaz.org>
2008-04-21 00:46:40 +0200

    Up: openbox (3.4.7.1) - Now Ob is the default WM

--- openbox/stuff/openbox/autostart.sh
+++ openbox/stuff/openbox/autostart.sh
@@ -1,24 +1,24 @@
 # This shell script is run before Openbox launches.
 # Environment variables set here are passed to the Openbox session.
 
-BG=/usr/share/images/slitaz-background.png
+#BG=/usr/share/images/slitaz-background.png
 #BG=$HOME/Images/background.png
 
 # Set a background color usin hsetroot (depends on imlib2) or xsetroot.
-if which hsetroot >/dev/null; then
-	hsetroot -fill $BG &
-else
-	if which xsetroot >/dev/null; then
-		xsetroot -solid "#222222" &
-	fi
-fi
+#if which hsetroot >/dev/null; then
+	#hsetroot -fill $BG &
+#else
+	#if which xsetroot >/dev/null; then
+		#xsetroot -solid "#222222" &
+	#fi
+#fi
 
-# Login sound.
-if which alsaplayer >/dev/null; then
-	alsaplayer -i text /usr/share/sounds/login.ogg &
+# Start PCmanFM as deamon
+if which pcmanfm >/dev/null; then
+	pcmanfm -d &
 fi
 
 # Start the panel.
 if which lxpanel >/dev/null; then
-	lxpanel --profile openbox &
+	lxpanel &
 fi
