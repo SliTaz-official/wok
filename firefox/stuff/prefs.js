9c2516a741fb4c983be3d7dd24aa6772fd9778ee
Christophe Lincoln <pankso@slitaz.org>
2008-06-07 00:48:02 +0200

    Up: firefox (3.0-rc2)

--- firefox/stuff/prefs.js
+++ firefox/stuff/prefs.js
@@ -9,5 +9,5 @@
  * For more information, see http://www.mozilla.org/unix/customizing.html#prefs
  */
 
-user_pref("general.useragent.extra.firefox", "Firefox/3.0rc1 (SliTaz GNU/Linux)");
+user_pref("general.useragent.extra.firefox", "Firefox/3.0rc2 (SliTaz GNU/Linux)");
 user_pref("intl.charsetmenu.browser.cache", "ISO-8859-1, UTF-8");
