96fdd27bc3ae7a96b1c7953d498e0954a4d820cf
Christophe Lincoln <pankso@slitaz.org>
2009-03-03 22:58:43 +0100

    Add slitaz-eeepc (tazeee, boot script, doc and more...)

---
+++ slitaz-eeepc/stuff/eeepc.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# /etc/init.d/eeepc.sh: Script used at boot time to setup screen 
+# resolution and configure hardware with tazeee on the EeePC.
+# 
+
+# Setup is run only once.
+if [ ! -s /etc/eeepc.conf ]; then
+	/sbin/tazeee setup
+fi
+
+. /etc/eeepc.conf
+
+# 915resolution screen hack.
+[ -n "$HACK_915" ] && 915resolution $HACK_915
+
+# Enable Laptop mode
+echo "5" > /proc/sys/vm/laptop_mode
+
+exit 0
