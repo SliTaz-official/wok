32c7ecf899e11843481412ce4062871bd0656a82
Eric Joseph-Alexandre <erjo@slitaz.org>
2009-07-13 10:52:36 +0200

    Fix: ocsinventory-agent, remove escape chars from tazpkg list output.

--- ocsinventory-agent/stuff/Tazpkg.pm
+++ ocsinventory-agent/stuff/Tazpkg.pm
@@ -6,18 +6,22 @@ use warnings;
 sub check { can_run("tazpkg") }
 
 sub run {
-  my $params = shift;
-  my $inventory = $params->{inventory};
+	my $params = shift;
+	my $inventory = $params->{inventory};
 
-# use dpkg-query -W -f='${Package}|||${Version}\n'
-  foreach (`tazpkg list `){
-    if (/^(\S+)\s+([0-9]+.*)\s+(.*)/) {
-      $inventory->addSoftwares({
-	  'NAME'          => $1,
-	  'VERSION'       => $2,
-	  });
-    }
-  }
+	# use tazpkg list\n'
+	foreach (`tazpkg list `){
+		next if (/List of/);
+		next if (/packages installed/);
+		
+		if (/^(\S+)\[24G\s+(\S+)\[42G\s+(\S+)/) {
+		  $inventory->addSoftwares({
+		  'NAME'    	=> $1,
+		  'VERSION'     => $2,
+		  'COMMENTS'	=> $3	
+		  });
+	   }
+	}
 
 }
 
