c905d92df761629533ee879283635d87d9ffeb8d
Eric Joseph-Alexandre <erjo@slitaz.org>
2010-01-21 13:17:18 +0100

    Fix: Add french langpack to seamonkey + switch to modern UI skin.

---
+++ seamonkey/stuff/prefs.js
@@ -0,0 +1,14 @@
+/*
+ * Default prefs.js for Seamonkey on SliTaz GNU/Linux.
+ *
+ * This file can be used to customize the look of Mozilla's user interface
+ * You should consider using !important on rules which you want to
+ * override default settings.
+ *
+ * For more examples see http://www.mozilla.org/unix/customizing.html
+ *
+ */
+
+user_pref("general.useragent.locale", "fr");
+user_pref("browser.startup.homepage", "file:///usr/share/webhome/index.html");
+user_pref("general.skins.selectedSkin", "modern/1.0");
