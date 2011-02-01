974bab6dbffce21a48cff5e32f60dd5f53647316
Antoine Bodin <gokhlayeh@slitaz.org>
2011-02-01 02:51:09 +0100

    Fix: check_module.sh (into linux stuff) modified to works with tazwok-experimental

--- linux/stuff/check_modules.sh
+++ linux/stuff/check_modules.sh
@@ -4,48 +4,48 @@
 # 20090618 <jozee@slitaz.org> 
 # 20100528 <pankso@slitaz.org>
 #
-. /etc/tazwok.conf
+WOK=$(cd `basedir $0` && pwd | sed 's/wok.*/wok/')
 VERSION=`grep  ^VERSION= $WOK/linux/receipt | cut -d "=" -f2 | sed -e 's/"//g'`
 src="$WOK/linux/linux-$VERSION"
 
 cd $src
-mkdir -p ../stuff/tmp
-rm -f ../stuff/tmp/*
+mkdir -p ../tmp
+rm -f ../tmp/*
 
 echo -e "\nChecking for modules selected in .config but not in linux-* pkgs"
 echo "======================================================================"
 
 # create a packaged modules list
-cat ../stuff/modules-"$VERSION".list >> ../stuff/tmp/pkgs-modules-"$VERSION".list 
+cat ../stuff/modules-"$VERSION".list >> ../tmp/pkgs-modules-"$VERSION".list 
 
 for i in $(cd $WOK; ls -d linux-*)
 do
 	tazpath="taz/$i-$VERSION"
 	for j in $(cat $WOK/$i/$tazpath/files.list | grep ".ko.gz")
 	do
-		basename $j >> ../stuff/tmp/pkgs-modules-"$VERSION".list	
+		basename $j >> ../tmp/pkgs-modules-"$VERSION".list	
 	done 	
 done
 # get the original list in .config
 for i in $(find $_pkg -iname "*.ko.gz") 
 do
-	basename $i >> ../stuff/tmp/originial-"$VERSION".list
+	basename $i >> ../tmp/originial-"$VERSION".list
 done
 # compare original .config and pkged modules
-for i in $(cat ../stuff/tmp/originial-$VERSION.list)   
+for i in $(cat ../tmp/originial-$VERSION.list)   
 do		
-	if ! grep -qs "$i" ../stuff/tmp/pkgs-modules-"$VERSION".list ; then 
+	if ! grep -qs "$i" ../tmp/pkgs-modules-"$VERSION".list ; then 
 		modpath=`find $_pkg -iname "$i"`
 		echo "Orphan module: $i"
-		echo "$i : $modpath" >> ../stuff/tmp/unpackaged-modules-"$VERSION".list
+		echo "$i : $modpath" >> ../tmp/unpackaged-modules-"$VERSION".list
 	fi
 done
-if [ -f ../stuff/tmp/unpackaged-modules-"$VERSION".list ]; then
+if [ -f ../tmp/unpackaged-modules-"$VERSION".list ]; then
 	echo "======================================================================"
-	echo -e "Check linux/stuff/tmp/unpackaged-modules-$VERSION.list for mod path\n"
+	echo -e "Check linux/tmp/unpackaged-modules-$VERSION.list for mod path\n"
 else
 	echo -e "\nAll modules are packaged\n"
 	echo "======================================================================"
 	echo ""
-	rm -rf ../stuff/tmp
+	rm -rf ../tmp
 fi
