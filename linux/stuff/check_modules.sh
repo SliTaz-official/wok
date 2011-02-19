344a0f312c12bbed43ca212be81e898d0af098c8
Christopher Rogers <slaxemulator@gmail.com>
2011-02-19 01:35:37 +0000

    Fixed linux not to put a tmp folder in WOK. Instead its WOK/PACKAGE/tmp now. This fixes a problem with update-repository dying out cause of WOK/tmp folder.

--- linux/stuff/check_modules.sh
+++ linux/stuff/check_modules.sh
@@ -12,43 +12,43 @@ VERSION=`grep  ^VERSION= $WOK/linux/receipt | cut -d "=" -f2 | sed -e 's/"//g'`
 src="$WOK/linux/linux-$VERSION"
 
 cd $src
-mkdir -p $WOK/tmp
-rm -f $WOK/tmp/*
+mkdir -p $WOK/$PACKAGE/tmp
+rm -f $WOK/$PACKAGE/tmp/*
 
 echo -e "\nChecking for modules selected in .config but not in linux-* pkgs"
 echo "======================================================================"
 
 # create a packaged modules list
-cat ../stuff/modules-"$VERSION".list >> $WOK/tmp/pkgs-modules-"$VERSION".list 
+cat ../stuff/modules-"$VERSION".list >> $WOK/$PACKAGE/tmp/pkgs-modules-"$VERSION".list 
 
 for i in $(cd $WOK; ls -d linux-*)
 do
 	tazpath="taz/$i-$VERSION"
 	for j in $(cat $WOK/$i/$tazpath/files.list | grep ".ko.gz")
 	do
-		basename $j >> $WOK/tmp/pkgs-modules-"$VERSION".list	
+		basename $j >> $WOK/$PACKAGE/tmp/pkgs-modules-"$VERSION".list	
 	done 	
 done
 # get the original list in .config
 for i in $(find $_pkg -iname "*.ko.gz") 
 do
-	basename $i >> $WOK/tmp/originial-"$VERSION".list
+	basename $i >> $WOK/$PACKAGE/tmp/originial-"$VERSION".list
 done
 # compare original .config and pkged modules
-for i in $(cat $WOK/tmp/originial-$VERSION.list)   
+for i in $(cat $WOK/$PACKAGE/tmp/originial-$VERSION.list)   
 do		
-	if ! grep -qs "$i" $WOK/tmp/pkgs-modules-"$VERSION".list ; then 
+	if ! grep -qs "$i" $WOK/$PACKAGE/tmp/pkgs-modules-"$VERSION".list ; then 
 		modpath=`find $_pkg -iname "$i"`
 		echo "Orphan module: $i"
-		echo "$i : $modpath" >> $WOK/tmp/unpackaged-modules-"$VERSION".list
+		echo "$i : $modpath" >> $WOK/$PACKAGE/tmp/unpackaged-modules-"$VERSION".list
 	fi
 done
-if [ -f $WOK/tmp/unpackaged-modules-"$VERSION".list ]; then
+if [ -f $WOK/$PACKAGE/tmp/unpackaged-modules-"$VERSION".list ]; then
 	echo "======================================================================"
 	echo -e "Check linux/tmp/unpackaged-modules-$VERSION.list for mod path\n"
 else
 	echo -e "\nAll modules are packaged\n"
 	echo "======================================================================"
 	echo ""
-	rm -rf $WOK/tmp
+	rm -rf $WOK/$PACKAGE/tmp
 fi
