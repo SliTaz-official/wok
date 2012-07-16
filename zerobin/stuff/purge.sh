4b9bbb59c666021b100ee2a46a9b1eddb6f087a9
Pascal Bellard <pascal.bellard@slitaz.org>
2012-07-16 10:19:42 +0200

    zerobin: add purge.sh (for crontab)

---
+++ zerobin/stuff/purge.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd $(dirname $0)
+for i in ?? ; do
+	for j in $i/?? ; do
+		for k in $j/* ; do
+			[ -f $k ] || continue
+			grep -qs '"expire_date":' $k || continue
+			[ $(sed 's/.*"expire_date":\([0-9]*\).*/\1/' $k) -lt \
+			  $(date +%s) ] && rm -rf $k*
+		done
+		rmdir $j 2> /dev/null
+	done
+	rmdir $i 2> /dev/null
+done
