1554641cee87f877fba78bacafa35109bfe9e406
Dominique Corbex <domcox@slitaz.org>
2013-03-16 21:28:06 +0100

    Up: firefox (17.0.4esr)

--- firefox/stuff/firefox-l10n.js
+++
@@ -1,5 +0,0 @@
-//@line 36 "/home/slitaz/experimental/wok/firefox/firefox-4.0/browser/locales/en-US/firefox-l10n.js"
-
-//@line 38 "/home/slitaz/experimental/wok/firefox/firefox-4.0/browser/locales/en-US/firefox-l10n.js"
-
-pref("general.useragent.locale", "en-US");
