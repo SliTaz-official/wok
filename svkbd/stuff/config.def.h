efdb147bc598ba8f8df46666d5e80d923cc74895
Christophe Lincoln <pankso@slitaz.org>
2014-04-18 10:51:46 +0200

    Add svkbd (realy nice and so small virtual keyboard) and up surf from suckless

---
+++ svkbd/stuff/config.def.h
@@ -0,0 +1,9 @@
+static const Bool wmborder = True;
+static const char font[] = "-misc-fixed-medium-r-semicondensed--0-0-75-75-c-0-iso8859-1";
+static const char normbgcolor[] = "#dfdfdf";
+static const char normfgcolor[] = "#000000";
+static const char pressbgcolor[] = "#111111";
+static const char pressfgcolor[] = "#ffffff";
+static const char highlightbgcolor[] = "#111111";
+static const char highlightfgcolor[] = "#ffffff";
+
