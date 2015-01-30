dbbfb5d80769542a1e9289015013be4598808a52
Pascal Bellard <pascal.bellard@slitaz.org>
2015-01-30 10:17:37 +0100

    Update some post_install for install --root

--- fatattr/stuff/fatattr.sh
+++ fatattr/stuff/fatattr.sh
@@ -9,7 +9,7 @@ cdfat() {
 case "${1/--/-}" in
 -s*)	find ${3:-.} -exec fatattr {} \; > ${2:-$ATTRS} ;;
 -r*)	sed -e 's/^\(.\{0,8\}\)/\1=/' -e ':a;s/^\([^ =]*\)\ /\1/;ta' \
-	    -e 's/^/fatattr +/;s/=//' < ${2:-$ATTRS} | sh ;;
+	    -e "s/^/fatattr +/;s|= | ${3:-.}/|" < ${2:-$ATTRS} | sh ;;
 -c*)	cdfat ${2:-.} && $0 -s && find . | cpio -o -H newc ;;
 -[xe]*)	cdfat ${2:-.} && cpio -idmu && $0 -r && rm -f $ATTRS ;;
 *)	cat 1>&2 <<EOT
