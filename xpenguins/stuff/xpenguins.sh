5a9db30b46c09fe1ef5a5973d8d978c8aeb133ff
Lucas Levrel <llevrel@yahoo.fr>
2016-01-14 22:11:10 +0100

    Add GUI to xsnow and xpenguins

---
+++ xpenguins/stuff/xpenguins.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+icon=--window-icon=/usr/share/pixmaps/xpenguins.png
+toons=$(xpenguins -l | yad --list --title="xpenguins settings (1/2)" $icon \
+	--text="Select all the toons you want to display" --height=330 \
+	--column="Theme name" --multiple --separator='')
+[ $? -eq 0 ] || exit 1
+[ -z $toons ] && toons=$(xpenguins -l)
+themes=''
+# double-quote theme names (some have spaces) then use xargs
+while read theme ; do themes=$themes' -t "'$theme'"' ; done <<EOT
+$toons
+EOT
+settings=$(LC_NUMERIC=C yad --form --title="xpenguins settings (2/2)" $icon \
+	--field="Animation update delay (ms) (0 = default)":NUM \
+	--field="Number of toons (-1 = default)":NUM \
+	--field="Ignore popup windows":CHK --field="Don't show any blood":CHK \
+	--field="Don't show any angel":CHK \
+	--field="Destroy toons with mouseclick":CHK \
+	--field="Duration (min) (0 = unlimited)":NUM \
+	-- '0' '-1!-1..256' false false false true 0)
+[ $? -eq 0 ] || exit 2
+pid1=$(pgrep -fl 'spacefm --desktop'|cut -f1 -d' ')
+pid2=$(pgrep -fl 'pcmanfm --desktop'|cut -f1 -d' ')
+[ -n "$pid1" -o -n "$pid2" ] && yad --title=Warning $icon \
+	--text="Your file manager windows will be closed, and your desktop icons \
+temporarily removed. Continue?"
+[ $? -eq 0 ] || exit 3
+[ -n "$pid1" ] && kill $pid1
+[ -n "$pid2" ] && kill $pid2
+#add dummy var to gobble last |
+IFS='|' read delay number nopopup noblood noangel squish duration dummy<<EOT
+$settings
+EOT
+flags=''
+[ $nopopup == TRUE ] && flags=$flags" -p"
+[ $noblood == TRUE ] && flags=$flags" -b"
+[ $noangel == TRUE ] && flags=$flags" -a"
+[ $squish == TRUE ] && flags=$flags" -s"
+printf '%s' "$themes" | xargs xpenguins -m ${delay%.*} -n ${number%.*} $flags \
+	>/dev/null 2>&1 &
+yad --button=gtk-stop --title='Stop xpenguins' $icon \
+	--timeout=$((60*${duration%.*}))
+pkill -x xpenguins
+alias pcmanfm='pcmanfm --desktop 2>/dev/null &'
+alias spacefm='spacefm --desktop 2>/dev/null &'
+# restart filemanager(s)
+if [ -n "$pid1" ]; then
+	if [ -n "$pid2" ]; then
+		# in case both were running
+		if [ $pid1 -lt $pid2 ]; then
+			spacefm
+			pcmanfm
+		else
+			pcmanfm
+			spacefm
+		fi
+	else
+		spacefm
+	fi
+elif [ -n "$pid2" ]; then
+	pcmanfm
+fi
---
+++ xsnow/stuff/xsnow.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+icon=--window-icon=/usr/share/pixmaps/xsnow.png
+# egrep -v '[A-Z]|gray|[0-9]$' /usr/share/X11/rgb.txt |cut -f 3 |tr '\n' '!'
+colors='snow!ghost white!white smoke!gainsboro!floral white!old lace!linen!antique white!papaya whip!blanched almond!bisque!peach puff!navajo white!moccasin!cornsilk!ivory!lemon chiffon!seashell!honeydew!mint cream!azure!alice blue!lavender!lavender blush!misty rose!white!black!dark slate grey!dim grey!slate grey!light slate grey!grey!light grey!midnight blue!navy!navy blue!cornflower blue!dark slate blue!slate blue!medium slate blue!light slate blue!medium blue!royal blue!blue!dodger blue!deep sky blue!sky blue!light sky blue!steel blue!light steel blue!light blue!powder blue!pale turquoise!dark turquoise!medium turquoise!turquoise!cyan!light cyan!cadet blue!medium aquamarine!aquamarine!dark green!dark olive green!dark sea green!sea green!medium sea green!light sea green!pale green!spring green!lawn green!green!chartreuse!medium spring green!green yellow!lime green!yellow green!forest green!olive drab!dark khaki!khaki!pale goldenrod!light goldenrod yellow!light yellow!yellow!gold!light goldenrod!goldenrod!dark goldenrod!rosy brown!indian red!saddle brown!sienna!peru!burlywood!beige!wheat!sandy brown!tan!chocolate!firebrick!brown!dark salmon!salmon!light salmon!orange!dark orange!coral!light coral!tomato!orange red!red!hot pink!deep pink!pink!light pink!pale violet red!maroon!medium violet red!violet red!magenta!violet!plum!orchid!medium orchid!dark orchid!dark violet!blue violet!purple!medium purple!thistle!dark grey!dark blue!dark cyan!dark magenta!dark red!light green'
+settings=$(LC_NUMERIC=C yad --form --columns=2 --title="xsnow settings" $icon \
+	--field="Number of snowflakes":NUM \
+	--field="Snow color":CB --field="Set background color":CHK \
+	--field="Background color":CB --field="Christmas trees color":CB \
+	--field="Santa Claus's size":NUM --field="Sleigh speed (-1 = default)":NUM \
+	--field="Sleigh refresh factor 1/":NUM --field="Snow update delay (ms)":NUM \
+	--field="Smooth snow movement":CHK --field="Snow whirling speed (max)":NUM \
+	--field="Snow drifting speed (max)":NUM \
+	--field="Snow falling speed (max)":NUM \
+	--field="Windy periods":CHK --field="Windy periods interval (s)":NUM \
+	--field="Maximal snow depth on windows":NUM \
+	--field="Maximal snow depth on screen bottom":NUM \
+	--field="Vertical offset of snow layers":NUM \
+	--field="Christmas trees":CHK --field="Santa Claus":CHK --field="Rudolf":CHK \
+	--field="Snow layers on windows":CHK \
+	--field="Snow layer on screen bottom":CHK \
+	--field="Check this if snow layering doesn't work":CHK \
+	--field="Duration (min) (0 = unlimited)":NUM \
+	-- '100!1..1000' "$colors" 'true' SkyBlue3\!"$colors" \
+	chartreuse\!"${colors/chartreuse/}" '2!0..2' '-1!-1..inf' '3!1..inf' \
+	50 true 3 2 '10!1..inf' true 30 15 50 0 true true true true true false 0)
+[ $? -eq 0 ] || exit 1
+pid1=$(pgrep -fl 'spacefm --desktop'|cut -f1 -d' ')
+pid2=$(pgrep -fl 'pcmanfm --desktop'|cut -f1 -d' ')
+[ -n "$pid1" -o -n "$pid2" ] && yad --title=Warning $icon \
+	--text="Your file manager windows will be closed, and your desktop icons \
+temporarily removed. Continue?"
+[ $? -eq 0 ] || exit 2
+[ -n "$pid1" ] && kill $pid1
+[ -n "$pid2" ] && kill $pid2
+# add dummy var to gobble last |
+IFS='|' read sf sc back bc tc ssiz ssp suf delay smooth wf xs ys wind wtimer \
+winsd scrsd off trees santa rud winsn scrsn popupsn duration dummy<<EOT
+$settings
+EOT
+[ $back == FALSE ] && back=''
+flags=''
+[ $smooth == FALSE ] && flags=$flags" -unsmooth"
+[ $wind == FALSE ] && flags=$flags" -nowind"
+[ $trees == FALSE ] && flags=$flags" -notrees"
+[ $santa == FALSE ] && flags=$flags" -nosanta"
+[ $rud == FALSE ] && flags=$flags" -norudolf"
+if [ $winsn == FALSE -a $scrsn == FALSE ]; then flags=$flags" -nokeepsnow"
+elif [ $winsn == FALSE ]; then flags=$flags" -nokeepsnowonwindows"
+elif [ $scrsn == FALSE ]; then flags=$flags" -nokeepsnowonscreen"
+fi
+[ $popupsn == TRUE ] && flags=$flags" -nonopopup"
+xsnow -snowflakes ${sf%.*} -sc "$sc" ${back:+-solidbg -bg "$bc"} -tc "$tc" \
+	-santa ${ssiz%.*} -santaspeed ${ssp%.*} -santaupdatefactor ${suf%.*} \
+	-delay ${delay%.*} -whirl ${wf%.*} -xspeed ${xs%.*} -yspeed ${ys%.*} \
+	-windtimer ${wtimer%.*} -wsnowdepth ${winsd%.*} -ssnowdepth ${scrsd%.*} \
+	-offset ${off%.*} $flags >/dev/null 2>&1 &
+yad --button=gtk-stop --title='Stop xsnow' $icon \
+	--timeout=$((60*${duration%.*}))
+pkill -x xsnow
+alias pcmanfm='pcmanfm --desktop 2>/dev/null &'
+alias spacefm='spacefm --desktop 2>/dev/null &'
+# restart filemanager(s)
+if [ -n "$pid1" ]; then
+	if [ -n "$pid2" ]; then
+		# in case both were running
+		if [ $pid1 -lt $pid2 ]; then
+			spacefm
+			pcmanfm
+		else
+			pcmanfm
+			spacefm
+		fi
+	else
+		spacefm
+	fi
+elif [ -n "$pid2" ]; then
+	pcmanfm
+fi
