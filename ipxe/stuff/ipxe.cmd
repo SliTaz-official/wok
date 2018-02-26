63928545e52d8baf2293d93aeaedc9a53dd13d34
Pascal Bellard <pascal.bellard@slitaz.org>
2018-02-26 11:01:43 +0100

    ipxe: fix ipxe.cmd

--- ipxe/stuff/ipxe.cmd
+++ ipxe/stuff/ipxe.cmd
@@ -8,15 +8,19 @@ console --picture http://mirror.slitaz.org/pxe/ipxe/slitaz.png ||
 menu SliTaz net boot menu
 item --key b boot	Local boot
 item --gap
+item --gap Configuration
+item --key e exit	iPXE command line
+item --key c config	iPXE configuration
+isset ${ip} || goto noip
+item --gap
 item --gap Network boot
-isset ${filename} && item --key l lan	Your PXE boot ${filename}
+isset ${filename} && item --key l lan	Your PXE boot ${filename} ||
 item --key w web	SliTaz WEB boot
 item --key r rolling	SliTaz development version
-item --gap
-item --gap Configuration
-isset ${ip} || item --key i ipset	IP settings
-item --key c config	iPXE configuration
-item --key e exit	iPXE command line
+goto endip
+:noip
+item --key i ipset	IP settings
+:endip
 choose --timeout ${menu-timeout} --default web target || goto exit
 set menu-timeout 0
 isset ${dns} || set dns 8.8.8.8
@@ -40,7 +44,6 @@ echo -n DNS server: ${} && read dns
 goto menu
 
 :web
-isset ${ip} || dhcp || echo No DHCP
 imgfree
 set weburl http://mirror.slitaz.org/pxe/
 chain --autofree ${weburl}ipxe/menu.ipxe && boot ||
@@ -57,12 +60,10 @@ chain --autofree ${weburl}ipxe/menu.ipxe && boot ||
 goto menu
 
 :lan
-isset ${ip} || dhcp || echo No DHCP
 autoboot ||
 goto menu
 
 :rolling
-isset ${ip} || dhcp || echo No DHCP
 sanboot http://mirror.slitaz.org/iso/rolling/slitaz-rolling.iso ||
 goto menu
 
